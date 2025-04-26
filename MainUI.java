6f312265f3b2f3dac248505f22c7b3733ff22523
kerem <keremelma388@outlook.com>
2025-04-27 00:35:24 +0300

    ui design complete

--- MainUI.java
+++ MainUI.java
@@ -4,8 +4,6 @@ import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
-
-import javax.security.auth.callback.TextInputCallback;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
@@ -74,5 +72,45 @@ public class MainUI implements Runnable {
             new Label("Errors:"),
             errorLogArea
         );
+
+        BorderPane upperHalfInput = new BorderPane();
+        upperHalfInput.setLeft(inputBox);
+        upperHalfInput.setRight(buttonBox);
+
+        BorderPane upperHalf = new BorderPane();
+        upperHalf.setLeft(upperHalfInput);
+        upperHalf.setRight(errorLogAreaBox);
+
+        VBox sendLogAreaBox = new VBox(
+            new Label("Sent Messages: "),
+            sendLogArea
+        );
+        VBox receiveLogAreaBox = new VBox(
+            new Label("Received Messages: "),
+            receiveLogArea
+        );
+        VBox logAreaBox = new VBox(
+            new Label("Information: "),
+            logArea
+        );
+
+        BorderPane lowerHalf = new BorderPane();
+        lowerHalf.setLeft(receiveLogAreaBox);
+        lowerHalf.setRight(sendLogAreaBox);
+        lowerHalf.setBottom(logAreaBox);
+
+        BordeerPane root = new BorderPane();
+        root.setStyle("-fx-background-color: #658ED3;");
+        root.setTop(upperHalf);
+        root.setBottom(lowerHalf);
+        root.setPadding(new Insets(20, 20, 20, 20));
+        startButton.setOnAction(event -> startConnection());
+        loadButton.setOnAction(event -> loadXmlData("config/config.xml"));
+
+        Scene scene = new Scene(root, 1280, 720);,
+        primaryStage.setTitle("UDP Messaging System");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
